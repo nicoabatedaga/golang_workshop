034caffb342f5311a4576300a3acbd510873e567
Nicolas Abatedaga <nabatedaga@dlocal.com>
2024-05-15 20:38:56 -0300

    implement a function a function with two return values and error handling

--- main.go
+++ main.go
@@ -4,6 +4,17 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	result, err := division(10, 2)
+	fmt.Println("Result:", result)
 
+	result, err = division(10, 0)
+	fmt.Println("Error:", err)
+}
+
+func division(a, b float64) (float64, error) {
+	fmt.Println(fmt.Sprintf("Division of %f by %f", a, b))
+	if b == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+	return a / b, nil
 }
