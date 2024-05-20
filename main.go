9e92ca8c5d9eff6ef85099f87e2dec9d05446127
Nicolas Abatedaga <nabatedaga@dlocal.com>
2024-05-20 19:32:50 -0300

    add person logic to main file

--- main.go
+++ main.go
@@ -1,20 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	Name      string
+	BirthDate time.Time
+}
+
+func NewPerson(name string, day int, month time.Month, year int) *Person {
+	return &Person{
+		Name:      name,
+		BirthDate: time.Date(year, month, day, 0, 0, 0, 0, time.UTC),
+	}
+}
+
+func (p *Person) Age() int {
+	now := time.Now()
+	years := now.Year() - p.BirthDate.Year()
+
+	if now.Month() < p.BirthDate.Month() || (now.Month() == p.BirthDate.Month() && now.Day() < p.BirthDate.Day()) {
+		years--
+	}
+
+	return years
+}
+
+func (p *Person) IsAdult() bool {
+	return p.Age() >= 18
+}
+
+func (p *Person) Presentation() string {
+	return fmt.Sprintf("hello my name is %s, i'm %d years old", p.Name, p.Age())
+}
 
 func main() {
 
-	result, err := division(10, 2)
-	fmt.Println("Result:", result)
-
-	result, err = division(10, 0)
-	fmt.Println("Error:", err)
-}
-
-func division(a, b float64) (float64, error) {
-	fmt.Println(fmt.Sprintf("Division of %f by %f", a, b))
-	if b == 0 {
-		return 0, fmt.Errorf("cannot divide by zero")
+	people := []*Person{
+		NewPerson("John", 1, time.January, 2000),
+		NewPerson("Nico", 1, time.January, 2010),
+		NewPerson("Jane", 1, time.January, 1990),
+		NewPerson("Alice", 1, time.January, 1980),
 	}
-	return a / b, nil
+
+	for _, p := range people {
+		fmt.Println(p.Presentation())
+		if p.IsAdult() {
+			fmt.Println("I'm an adult")
+		} else {
+			fmt.Println("I'm not an adult")
+		}
+	}
+
 }
