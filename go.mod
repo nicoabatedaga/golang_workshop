583f7f7bd1b8e480ff6afda4713fd5df7425dd86
Nicolas Abatedaga <nabatedaga@dlocal.com>
2024-05-20 19:44:37 -0300

    go mod init, and separe Person to another module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nicoabatedaga/golang_workshop
+
+go 1.21.5
--- main.go
+++ main.go
@@ -2,47 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/nicoabatedaga/golang_workshop/models"
 	"time"
 )
 
-type Person struct {
-	Name      string
-	BirthDate time.Time
-}
-
-func NewPerson(name string, day int, month time.Month, year int) *Person {
-	return &Person{
-		Name:      name,
-		BirthDate: time.Date(year, month, day, 0, 0, 0, 0, time.UTC),
-	}
-}
-
-func (p *Person) Age() int {
-	now := time.Now()
-	years := now.Year() - p.BirthDate.Year()
-
-	if now.Month() < p.BirthDate.Month() || (now.Month() == p.BirthDate.Month() && now.Day() < p.BirthDate.Day()) {
-		years--
-	}
-
-	return years
-}
-
-func (p *Person) IsAdult() bool {
-	return p.Age() >= 18
-}
-
-func (p *Person) Presentation() string {
-	return fmt.Sprintf("hello my name is %s, i'm %d years old", p.Name, p.Age())
-}
-
 func main() {
 
-	people := []*Person{
-		NewPerson("John", 1, time.January, 2000),
-		NewPerson("Nico", 1, time.January, 2010),
-		NewPerson("Jane", 1, time.January, 1990),
-		NewPerson("Alice", 1, time.January, 1980),
+	people := []*models.Person{
+		models.NewPerson("John", 1, time.January, 2000),
+		models.NewPerson("Nico", 1, time.January, 2010),
+		models.NewPerson("Jane", 1, time.January, 1990),
+		models.NewPerson("Alice", 1, time.January, 1980),
 	}
 
 	for _, p := range people {
