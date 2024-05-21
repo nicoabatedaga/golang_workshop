e87e300d978a63f9fde3dcd919edc2dd71f0832b
Nicolas Abatedaga <nabatedaga@dlocal.com>
2024-05-20 21:36:47 -0300

    add mutex and multi gorutines modifying the same memory address

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"github.com/pkg/profile"
+	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	defer profile.Start(profile.ProfilePath(".")).Stop()
+	// defer profile.Start(profile.ProfilePath(".")).Stop()
 
 	c := make(chan int)
+	var m sync.Mutex
+	x, y := 0, 1
 
-	go fibonacci(c)
+	go fibonacci(c, &x, &y, 1, &m)
+	go fibonacci(c, &x, &y, 2, &m)
+
 	go func() {
 		time.Sleep(5 * time.Second)
 		close(c)
@@ -22,12 +27,28 @@ func main() {
 
 }
 
-func fibonacci(c chan int) {
-	x, y := 0, 1
+func fibonacci(c chan int, x, y *int, processID int, m *sync.Mutex) {
 	for {
-		c <- x
-		x, y = y, x+y
+		m.Lock()
+		if IsClosed(c) {
+			m.Unlock()
+			break
+		}
+		println(fmt.Sprintf("Process %d -> %d", processID, *x))
+		c <- *x
+		*x, *y = *y, *x+*y
+		m.Unlock()
 		// wait
 		time.Sleep(500 * time.Millisecond)
 	}
+}
+
+func IsClosed(ch <-chan int) bool {
+	select {
+	case <-ch:
+		return true
+	default:
+	}
+
+	return false
 }
