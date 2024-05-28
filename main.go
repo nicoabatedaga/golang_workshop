190f79b47679279107279c8efea1a8b259170070
Nicolas Abatedaga <nabatedaga@dlocal.com>
2024-05-28 19:17:30 -0300

    add example with select

--- main.go
+++ main.go
@@ -1,42 +1,30 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	// ctx, cancel := context.WithTimeout(context.Background(), 2600*time.Millisecond)
-	// ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(3*time.Second))
+	c1 := make(chan string)
+	c2 := make(chan string)
 
-	result := make(chan string)
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "uno"
+	}()
 
-	startTime := time.Now()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "dos"
+	}()
 
-	go doHttpCall(ctx, result, 1, 3)
-	go doHttpCall(ctx, result, 2, 2)
-	go doHttpCall(ctx, result, 3, 1)
-	go doHttpCall(ctx, result, 4, 6)
-	msg := <-result
-	cancel()
-
-	elapsed := time.Since(startTime)
-	fmt.Println("Total time: ", elapsed)
-	fmt.Println("Message: ", msg)
-
-	time.Sleep(10 * time.Second)
-	// cancel()
-}
-
-func doHttpCall(ctx context.Context, result chan string, process int, seconds int) {
-	start := time.Now()
-	select {
-	case <-ctx.Done():
-		fmt.Println(fmt.Sprintf("Process %d cancelled, elapsed time: %v", process, time.Since(start)))
-	case <-time.After(time.Duration(seconds) * time.Second):
-		println(fmt.Sprintf("Process %d finished, elapsed time: %v", process, time.Since(start)))
-		result <- fmt.Sprintf("Finished process %d", process)
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("Recibido", msg1)
+		case msg2 := <-c2:
+			fmt.Println("Recibido", msg2)
+		}
 	}
 }
